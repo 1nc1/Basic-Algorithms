937c74c2eabfb562676072de9cc354bc2c09112b
1nc1 <1nc1github@gmail.com>
2019-08-27 01:05:15 -0700

    Finished Algorithms I

---
+++ Basic-Algos-IV.js
@@ -0,0 +1,71 @@
+// Arrays Reintroduction
+
+// 1.
+
+// function one(arr,y) {
+//     var count = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] > y) {
+//             console.log(arr[i]);
+//             count++;
+//         }
+//     }
+//     console.log('Count is:',count);
+// }
+
+// one([76,25,23,4,13,25],20);
+
+// ---------------------------------------------
+// 2.
+
+// function two(arr) {
+//     var max = arr[0];
+//     var min = arr[0];
+//     var sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//         if(arr[i] > max) {
+//             max = arr[i];
+//         } else if(arr[i] < min) {
+//             min = arr[i];
+//         }
+//     }
+//     console.log(max);
+//     console.log(min);
+//     console.log(sum / arr.length);
+// }
+
+// two([6,2,8,4,13]);
+
+// ---------------------------------------------
+// 3.
+
+// function three(arr) {
+//     newarr = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] < 0) {
+//             arr[i] = 'Dojo';
+//         }
+//     }
+//     newarr.push(arr);
+//     return newarr;
+// }
+
+// console.log(three([1,2,-3,-5,5]));
+
+// ---------------------------------------------
+// 4.
+
+// function four(arr,start,end) {
+//     var diff = (end - start) + 1;
+//     for(let i = start; i <= diff; i++) {
+//         arr[i] = arr[i + diff];
+//     }
+//     console.log(arr);
+//     arr.length = arr.length - diff;
+//     return arr;
+// }
+
+// console.log(four([20,30,40,50,60,70],2,4));
+
+
---
+++ Basic-Algos-V.js
@@ -0,0 +1,54 @@
+// More Arrays
+
+// 1.
+
+// function one(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] < 0) {
+//             arr[i] = 0;
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(one([1,2,-1,-3]));
+
+// ---------------------------------------------
+// 2.
+
+// function two(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i + 1];
+//     }
+//     arr[arr.length - 1] = 0;
+//     return arr;
+// }
+
+// console.log(two([1,2,3]));
+
+// ---------------------------------------------
+// 3.
+
+// function three(arr) {
+//     var newarr = [];
+//     for(let i = arr.length - 1; i >= 0; i--) {
+//         newarr.push(arr[i]);
+//     }
+//     return newarr;
+// }
+
+// console.log(three([1,2,3]));
+
+// ---------------------------------------------
+// 4.
+
+// function four(arr) {
+//     var newarr = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         newarr.push(arr[i]);
+//         newarr.push(arr[i]);
+//     }
+//     return newarr;
+// }
+
+// console.log(four([4,'Ulysses',42,false]));
