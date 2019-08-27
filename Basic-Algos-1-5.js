ae22c1c0a81fe5ff71a7028e087132ea286f3bac
1nc1 <1nc1github@gmail.com>
2019-08-26 20:44:51 -0700

    Finished Algorithms I

---
+++ Basic-Algos-1-5.js
@@ -0,0 +1,97 @@
+// Algorithms I
+// --------------------------------------------------------------
+// var x = [1,2,3,4,5,10];
+// for(var i = 0; i < 5;i++){   
+//    console.log(i);
+// }
+
+// Predicted Output:
+// 0,1,2,3,4
+
+// Actual Output:
+// 0,1,2,3,4
+
+// --------------------------------------------------------------
+// var x = [1,2,3,4,5,10];
+// for(var i = 0; i < 5; i++) {
+//     i = i + 1;
+//     console.log(i);
+// }
+
+// Predicted Output:
+// 1,3,5
+
+// Actual Output:
+// 1,3,5
+
+// --------------------------------------------------------------
+// var x = [1,2,3,4,5,10];
+// for(var i = 0; i < 5; i++) {
+//     i = i + 3;
+//     console.log(i);
+// }
+
+// Predicted Output:
+// 3,7
+
+// Actual Output:
+// 3,7
+
+// --------------------------------------------------------------
+// function y(num1, num2) {   
+//     return num1+num2;
+//  }
+//  console.log(y(2,3))
+//  console.log(y(3,5))
+
+// Predicted Output:
+// 5,8
+
+// Actual Output:
+// 5,8
+
+// --------------------------------------------------------------
+// function y(num1, num2){
+//     console.log(num1);   
+//     return num1+num2;
+//  }
+//  console.log(y(2,3))
+//  console.log(y(3,5))
+
+// Predicted Output:
+// 2,5,3,8
+
+// Actual Output:
+// 2,5,3,8
+
+// --------------------------------------------------------------
+// a = 15;
+// console.log(a);
+// function y(a){
+//    console.log(a);   
+//    return a;
+// }
+// b = y(10);
+// console.log(b);
+
+// Predicted Output:
+// 15,10,10
+
+// Actual Output:
+// 15,10,10
+
+// --------------------------------------------------------------
+// a = 15;
+// console.log(a);
+// function y(a){
+//    console.log(a);   
+//    return a*2;
+// }
+// b = y(10);
+// console.log(b);
+
+// Predicted Output:
+// 15,10,20
+
+// Actual Output:
+// 15,10,20
